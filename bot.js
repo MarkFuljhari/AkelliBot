0052b935f2a428dd7eb6935c4dc8acaab654e2f4
MarkFuljhari <45743552+MarkFuljhari@users.noreply.github.com>
2019-01-26 13:11:56 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,11 @@
 // This two lines are constants the stuff that you need in order for bot to run should be all declared on the begging of the code
+
 const Discord = require('discord.js'); //This one tells you that constant with the name Discord in order to work requires discord.js library
-const client = new Discord.Client(); //This one here tells you that constant with the name client in order to work requires a new Discord client which basicly means that it creates a new client/bot/user
-// This is prefix beeing used in front of the commands for Example: !akelli ping here !akelli is the prefix and ping is the command that had been used
-var prefix = "!akelli ";
+
+const client = new Discord.Client();   //This one here tells you that constant with the name client in order to work requires a new Discord client which basicly means that it creates a new client/bot/user
+
+// BOT PREFIX:
+var prefix = "a!";
 
 // GREET COMMANDS
 client.on('guildMemberAdd', member => {
@@ -15,6 +18,7 @@ client.on('guildMemberAdd', member => {
     guild.defaultChannel.sendMessage(`${member} has left the server!`);
   });
 
+// CONSOLE CODE:
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     console.log('I am online !');
@@ -28,12 +32,12 @@ client.on('message', async msg => {
     const args = msg.content.slice(prefix.length).trim().split(/ +/g);
     var argresult = args.join(' ');
     
- // EXAMPLE COMMAND    
+ // EXAMPLE COMMAND INSTRUCTION & MEANING:    
  // if (msg.content === 'example') { // the { here signs the start of the scope(of the if sentence)
 //    msg.reply('This is an example command!');
 //  } // the } here signs the end of the scope(if sentence)   
     
-  if(msg.author.bot) return; //This here means that if message was sent from the other bots expect Akelli, our bot(Akelli) won't sent anything in the chat(thats whay return is there e.g return, returns you to somewhere or something) this prevents bot-ception(when multiple bots were to use same prefix)
+  if(msg.author.bot) return; //This here means that if message was sent from the other bots EXEPT Akelli, our bot (Akelli) won't sent anything in the chat (thats why return there is e.g return, returns you to somewhere or something) this prevents bot-ception (when multiple bots were to use the same prefix).
 
   if(msg.content.indexOf(prefix) !== 0) return;
     
@@ -45,8 +49,8 @@ client.on('message', async msg => {
 // BOT HELP COMMAND - MISC CMDS
  if (msg.content.startsWith(prefix + 'help')) {
     msg.channel.send({embed:{
-  "title": "My commands:",
-      "description": "**-----------------------------**\n**»** `Server Owner (Level 3)`\nHas access to all of the commands.\n**»** `Server Administrator (Level 2)`\n ban, unban, kick.\n**»** `Server Moderator (Level 1)`\n mute, unmute, warn.\n» `Server Member (Level 0)`\n help, ping, avatar, play, search, stop.\n**-----------------------------**\n\nExpect more commands to be added soon upon future development !",
+  "title": "Commands: (usage: a!<command>",
+      "description": "》:notes:Music:\n  COMING SOON \n\n》:chart_with_upwards_trend:Administration:\navatar <@user> - Sends you the user's profile picture. \nmyavatar - Sends you your profile picture. \n\n》:robot: Information:\nhelp - Shows this message. \nping - Displays bot's latency and DiscordApp API \n\n》:office: Akelli Company: \nsetgame - Changes the game activity status of the bot.",
       "color": 6402937,
       "footer": {
         "text": "©? Copyright Akelli Company 2019"
@@ -59,6 +63,8 @@ client.on('message', async msg => {
 }
     });
 }
+    
+    
 // USER INFO & AVATAR COMMANDS - MISC CMDS
     if (msg.content.startsWith(prefix + 'myavatar')){
     msg.member.send(msg.author.avatarURL);
@@ -70,7 +76,9 @@ client.on('message', async msg => {
     msg.member.send(member.user.displayAvatarURL);
     msg.reply('I have sent you a private message!??');
   }
-// BOT INFORMATION - MISC CMDS
+    
+    
+//  INFORMATION COMMANDS - MISC CMDS
   if (msg.content.startsWith(prefix + 'social')){
   msg.channel.send({embed:{
     "plainText": "**My Social Media:**",
@@ -105,13 +113,13 @@ client.on('message', async msg => {
 }
  // ADMIN & MODERATOR COMMANDS - AkelliCompany MANAGEMENT & STAFF ONLY
     if (msg.content.startsWith(prefix + 'setgame')) {
-    if(!msg.member.roles.some(r=>["Akelli Admin","Akelli Moderator"].includes(r.name)) )
-    return msg.reply("error! You don't have sufficient access to execute this command! ?? \n Requirement: Management Team or Staff Moderator");
+    if(!msg.member.roles.some(r=>["AC Management","AkelliBot Mod"].includes(r.name)) )
+    return msg.reply("error! You don't have sufficient access to execute this command! \n Requirement: Management Team or Staff Moderator");
 
     let result = args.slice(1).join(' ');
     
     client.user.setActivity(result);
-    msg.reply("command accepted! ? \nNew game activity message set!");
+    msg.reply("command accepted! \nNew game activity message set!");
   }
     
 });
