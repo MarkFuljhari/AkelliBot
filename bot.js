df632be9dc549c823411b6e865792b9351976602
MarkFuljhari <45743552+MarkFuljhari@users.noreply.github.com>
2019-01-26 17:46:05 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,19 +50,17 @@ client.on('message', async msg => {
  if (msg.content.startsWith(prefix + 'help')) {
     msg.channel.send({embed:{
   "title": "Commands: (usage: a!<command>)",
-"plainText": "Online",
-  "title": "Akelli",
-  "description": "**» Music:** \n  COMING SOON \n\n**» Administration:**\navatar <@user> - Sends you the user's profile picture. \nmyavatar - Sends you your profile picture. \n\n**» Information:**\nhelp - Shows this message. \nping - Displays bot's latency and DiscordApp API \nsocialmedia - Shows Team\'s social media. \n\n**» Akelli Company:** \nsetgame - Changes the game activity status of the bot.",
-  "author": {
-    "icon_url": "https://cdn.discordapp.com/avatars/534003592542027786/10199bbc68d2c69dea28e76772e175ba.png?size=2048"
-  },
-  "color": 15105570,
-  "footer": {
-    "text": "© Copyright Akelli Company 2019, @akellicompany",
-    "icon_url": "https://cdn.discordapp.com/avatars/534003592542027786/10199bbc68d2c69dea28e76772e175ba.png?size=2048"
-  },
-  "thumbnail": "https://cdn.discordapp.com/attachments/344215127828070401/538810804024377374/20190117_091141.jpg"
-  }
+      "description": "**» Music:** \n  COMING SOON \n\n**» Administration:**\navatar <@user> - Sends you the user's profile picture. \nmyavatar - Sends you your profile picture. \n\n**» Information:**\nhelp - Shows this message. \nping - Displays bot's latency and DiscordApp API \nsocialmedia - Shows Team\'s social media. \n\n**» Akelli Company:** \nsetgame - Changes the game activity status of the bot.",
+      "color": 3447003,
+      "footer": {
+        "text": "© Copyright Akelli Company 2019, @akellicompany"
+        
+      },
+      "author": {
+        "name": "Akelli",
+        "url": "https://thebestgameryt.github.io/AkelliBot/",
+        "icon_url": "https://cdn.discordapp.com/avatars/534003592542027786/10199bbc68d2c69dea28e76772e175ba.png?size=2048"
+      }
 }
     });
 }
