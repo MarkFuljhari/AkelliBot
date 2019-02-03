beb38b762d8555a8b928585cfd5237f8a4606baf
MarkFuljhari <45743552+MarkFuljhari@users.noreply.github.com>
2019-02-02 21:59:02 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js'); //This one tells you that constant with t
 
 const client = new Discord.Client();   //This one here tells you that constant with the name client in order to work requires a new Discord client which basicly means that it creates a new client/bot/user
 
-//// BOT PREFIX: EXAMPLE: !help , a!help, $help , b$help , b!help etc. ////
+// BOT PREFIX:
 var prefix = "a!";
 
 //// GREET COMMAND SECTION ////
@@ -13,45 +13,45 @@ var prefix = "a!";
     client.on('guildMemberAdd', member => {
     let guild = member.guild;
     guild.defaultChannel.sendMessage(`Everyone, welcome ${member} to the server!`);
-    });
+  });
 
     // USER LEAVES SERVER
     client.on('guildMemberRemove', member => {
     let guild = member.guild;
     guild.defaultChannel.sendMessage(`${member.user.username} has left the server!`);
-    });
+  });
 
 //// CONSOLE REPLY & GAME ACTIVITY STATUS OF THE BOT: ////
-
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    console.log('I am ready & online on DiscordApp!');
+    console.log('I am online !');
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
     client.user.setActivity(`a!help | ${client.guilds.size} Servers`, { type: 'PLAYING' })
     .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : `a!help | ${client.guilds.size} Servers`}`))
     .catch(console.error);
   });
 
-//// SHORTCUT TEXTS IN THE JAVASCRIPT (CODE): ////
-
 client.on('message', async msg => {
     const args = msg.content.slice(prefix.length).trim().split(/ +/g);
     var argresult = args.join(' ');
 
-     // EXAMPLE COMMAND INSTRUCTION & MEANINGS:
-     // if (msg.content === 'example') { // the { here signs the start of the scope(of the if sentence)
-     //    msg.reply('This is an example command!'); }
-     //  the } here signs the end of the scope(if sentence)
+// EXAMPLE COMMAND INSTRUCTION & MEANING:
+// if (msg.content === 'example') { // the { here signs the start of the scope(of the if sentence)
+//    msg.reply('This is an example command!');
+//  } // the } here signs the end of the scope(if sentence)
+
 
   if(msg.author.bot) return; //This here means that if message was sent from the other bots EXEPT Akelli, our bot (Akelli) won't sent anything in the chat (thats why return there is e.g return, returns you to somewhere or something) this prevents bot-ception (when multiple bots were to use the same prefix).
+
   if(msg.content.indexOf(prefix) !== 0) return;
+
 
 //// HELP COMMAND (EMBED MESSAGE: https://embedbuilder.nadekobot.me/) ////
 
  if (msg.content.startsWith(prefix + 'help')) {
     msg.channel.send({embed:{
   "title": "Commands: (usage: a!<command>) Join server: https://discord.gg/j39xBQa",
-      "description": "Hi! I'm AkelliBot! \nI am 24/7 online & being developed by:\nAkelli Company's Developers (TeamCo7Clan) \nFor a list of commands enter **a!commands** \n\nDo you wish to add me in your server? \nEnter **a!invite** \n\nNeed support? Join our server!\nhttps://discord.gg/j39xBQa",
+      "description": "**» Music:** \n  COMING SOON \n\n**» Administration:**\navatar <@user> - Sends you the user's profile picture. \nmyavatar - Sends you your profile picture. \n\n**» Information:**\nhelp - Shows this message. \ninvite - Link to invite Bot!\nping - Displays bot's latency and DiscordApp API \nsocialmedia - Shows Team\'s social media. \n\n**» Akelli Company:** \nsetgame - Changes the game activity status of the bot.",
       "color": 3447003,
       "footer": {
         "text": "© Copyright Akelli Company 2019, @akellicompany"
@@ -79,7 +79,7 @@ client.on('message', async msg => {
     if (msg.content.startsWith(prefix + 'avatar')){
     let member = msg.mentions.members.first();
     msg.member.send(member.user.displayAvatarURL);
-    msg.reply('user\'s profile picture sent to you!');
+    msg.reply('I have sent you a private message!');
   }
 
 
@@ -96,7 +96,7 @@ client.on('message', async msg => {
   },
   "color": 15105570,
   "footer": {
-    "text": "© Copyright Akelli Company 2019",
+    "text": "© Copyright Akelli Company 2019, @akellicompany",
     "icon_url": "https://cdn.discordapp.com/avatars/534003592542027786/10199bbc68d2c69dea28e76772e175ba.png?size=2048"
   },
   "thumbnail": "https://cdn.discordapp.com/attachments/344215127828070401/538810804024377374/20190117_091141.jpg",
@@ -122,10 +122,10 @@ client.on('message', async msg => {
 }
 
      // PING CMD
-     if (msg.content.startsWith(prefix + 'ping')) {
-     const m = await msg.channel.send("Pong! Checking status");
-     m.edit(`Pong! Latency: **${m.createdTimestamp - msg.createdTimestamp}**ms. API Latency: **${Math.round(client.ping)}**ms`);
-    }
+if (msg.content.startsWith(prefix + 'ping')) {
+  const m = await msg.channel.send("Pong! Checking status");
+  m.edit(`Pong! Latency: **${m.createdTimestamp - msg.createdTimestamp}**ms. API Latency: **${Math.round(client.ping)}**ms`);
+}
 
     // BOT INVITATION CMD
     if (msg.content.startsWith(prefix + 'invite')){
@@ -150,12 +150,14 @@ client.on('message', async msg => {
     // SETGAME <MESSAGE> CMD
     if (msg.content.startsWith(prefix + 'setgame')) {
     if(!msg.member.roles.some(r=>["AC Management","Akelli Staff"].includes(r.name)) )
-    return msg.reply("you don't have sufficient access to execute this command! \n Requirement: Management Team or Staff Moderator");
+    return msg.reply("error! You don't have sufficient access to execute this command! \n Requirement: Management Team or Staff Moderator");
 
     let result = args.slice(1).join(' ');
 
     client.user.setActivity(result);
     msg.reply("command accepted! \nNew game activity message set!");
   }
+
+});
 
 client.login(process.env.BOT_TOKEN);
