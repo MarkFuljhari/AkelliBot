d5b72092708fc661b36f9d0feda5a5d5ab9aa1ee
MarkFuljhari <45743552+MarkFuljhari@users.noreply.github.com>
2019-02-23 23:17:53 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -73,7 +73,7 @@ client.on('guildMemberAdd', member => {
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     console.log('Success! I am online on DiscordApp.');
-    console.log(`Bot has woken up, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+    console.log(`Bot has woken up! With ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} servers.`);
     client.user.setActivity(`=help | ${client.guilds.size} servers`, { type: 'PLAYING' })
     .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : `=help | ${client.guilds.size} servers`}`))
     .catch(console.error);
